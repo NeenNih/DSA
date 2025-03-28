cf975ca91fa40d88dfe172aac24c0522e6ca6e64
NeenNih <neen.nih@gmail.com>
2025-03-28 15:10:21 +0600

    Add files via upload

---
+++ DSA_Selection Sort/src/Main.java
@@ -0,0 +1,44 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int array[] = {25, 37, 12, 87, 98, 34, 54, 65, 14, 102, 58, 42};
+		
+		selectionSort(array);
+			
+		for(int i : array) { 
+			
+			System.out.print(i + " ");
+			
+		}
+		
+	}
+
+	private static void selectionSort(int array[]) {
+		
+		for(int i = 0; i < array.length - 1; i++) {
+			
+			int min = i;
+			
+			for(int j = i + 1; j < array.length; j++) {
+				
+				if(array[min] > array[j]) {
+					
+					min = j;
+						
+				}
+				
+			}
+			
+			int temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			
+		}
+		
+		
+	}
+
+}
