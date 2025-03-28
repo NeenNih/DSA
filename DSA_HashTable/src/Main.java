e29ddc890c84ec4338bb7aeac32d2082003eadac
NeenNih <neen.nih@gmail.com>
2025-03-28 15:03:03 +0600

    Add files via upload

---
+++ DSA_HashTable/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+	
+	 public static void main(String args[]) {    	
+		 
+		   	Hashtable<String, String> table = new Hashtable<>(10);
+	    	
+	    	table.put("100", "Spongebob");
+	    	table.put("123", "Patrick");
+	    	table.put("321", "Sandy");
+	    	table.put("555", "Squidward");
+	    	table.put("777", "Gary");
+	    	  	
+	    	for(String key : table.keySet()) {
+	    		
+	    		System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + table.get(key));
+	    		
+	    	}
+		 
+	 }
+
+}
+
+
+
+
+
+
